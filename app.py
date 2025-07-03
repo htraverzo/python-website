5dc0b3f86bb286b1bc0ed03f008474d338055641
htraverzo <84256474+htraverzo@users.noreply.github.com>
2025-07-03 16:32:15 +0000

    Added Dynamic data and API route

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+COURSE = [{
+    'id': 1,
+    'title': 'Data Structures',
+    'description': 'Learn about data structures',
+    'price': '3000',
+}, {
+    'id': 2,
+    'title': 'Data Analitycs',
+    'description': 'Learn about data analytics',
+}, {
+    id: 3,
+    'title': 'Data Sciences',
+    'description': 'Learn about data Sciences',
+    'price': '3000',
+}]
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
+
+@app.route("/")
+def project_python():
+    return render_template('home.html', courses=COURSE)
+
+
+@app.route("/api/courses")
+def list_courses():
+    return jsonify(COURSE)
 
 
 if __name__ == '__main__':
